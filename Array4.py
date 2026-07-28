0306d89566a82a4ef1a15c6cf2cd5f2c19802489
Darshan K G <kgdarshan2005@gmail.com>
2026-07-28 19:33:41 +0530

    Different types of matrix

---
+++ Array4.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+ones=np.ones([3,3])
+print("Ones Array:\n",ones)
+
+Zeros=np.zeros([2,2])
+print("Zeros Array:\n",Zeros)
+
+full=np.full([3,3],12/2)
+print("Full Array:\n",full)
+
+random=np.random.random([3,3])
+print("Random Array:\n",random)
+
+sequence=np.arange(1,6)
+print("\n",sequence)
