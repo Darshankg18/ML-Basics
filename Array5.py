3340666e8fccc697301803e2ff18ede02cffe5c7
Darshan K G <kgdarshan2005@gmail.com>
2026-07-29 19:49:43 +0530

    Vector,Matrix,Tensor

---
+++ Array5.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+Vector=np.array([1,2,3,4,5])
+print("\n",Vector)
+
+Matrix=np.array([[10,20,30],[40,50,60]])
+print("\n",Matrix)
+
+Tensor=np.array([[[1,2,3],[4,5,6]],[[1,2,3],[1,2,3]]])
+print("\n",Tensor)
