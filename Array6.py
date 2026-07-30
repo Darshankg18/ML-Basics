6c58856072fb6666b004b9ea75883849dc1c528a
Darshan K G <kgdarshan2005@gmail.com>
2026-07-30 18:56:14 +0530

    Array Properties

---
+++ Array6.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array=np.array([[10,20,30],[40,50,60]])
+
+print("Shape:",array.shape)
+print("Dimension:",array.ndim)
+print("Size:",array.size)
+print("Datatype:",array.dtype)
+
+
+
