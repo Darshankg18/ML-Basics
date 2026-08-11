96be7810718d6c56882cbb27a979f90022ec45e5
Darshan K G <kgdarshan2005@gmail.com>
2026-08-11 22:59:56 +0530

    Data cleaning in pandas

---
+++ Data_cleaning.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+df=pd.read_csv("data.csv")
+df=df.drop(columns=["No"])
+df=df.dropna(subset=["Type2"])
+df=df.fillna({"Type2":"None"})
+df["Type"]=df["Type1"].replace({"Grass":"GRASS"})
+df["Name"]=df['Name'].str.lower()
+df["Legendary"]=df["Legendary"].astype(bool)
+df=df.drop_duplicates()
+print(df)
