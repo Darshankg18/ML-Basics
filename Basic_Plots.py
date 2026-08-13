5a42a56897c34555a3ac85f5a5964626b6918f74
Darshan K G <kgdarshan2005@gmail.com>
2026-08-13 22:12:32 +0530

    Basic plots of matplotlib

---
+++ Basic_Plots.py
@@ -0,0 +1,13 @@
+## BASIC PLOTs in matplotlib
+# import matplotlib.pyplot as plt
+# x=[21,33,45,18,69]
+# y=[12,25,34,76,89]
+# plt.plot(x,y)
+# plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.array([21,33,45,18,69])
+y=np.array([12,25,34,76,89])
+plt.plot(x,y)
+plt.show() 
