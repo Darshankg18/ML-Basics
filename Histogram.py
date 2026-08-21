545828263f0a64e26c559acec39e98a499c9512a
Darshan K G <kgdarshan2005@gmail.com>
2026-08-21 22:04:52 +0530

    Histogram in matplotlib

---
+++ Histogram.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+scores=np.random.normal(loc=80,scale=10,size=100)
+scores=np.clip(scores,0,100)
+plt.hist(scores,bins=10,color="skyblue",edgecolor="black")
+plt.title("Exam scores")
+plt.xlabel("Score")
+plt.ylabel("Students")
+plt.show()
